7e7a0dc964bf11703ea16310a8bd1b66036330b0
WTGConner <WTGConner@users.noreply.github.com>
2017-06-25 22:48:21 +0200

    added cocaïne command and item

---
+++ GHGDrugs/src/nl/gewoonhdgaming/commands/CommandCoke.java
@@ -0,0 +1,31 @@
+package nl.gewoonhdgaming.commands;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class CommandCoke implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String lavel, String[] args) {
+		if(sender.hasPermission("drugs.coke")) {
+			if(sender instanceof Player) {
+				ItemStack stack = new ItemStack(Material.SUGAR);
+				ItemMeta meta = stack.getItemMeta();
+				meta.setDisplayName("§fCocaïne");
+				stack.setItemMeta(meta);
+				((Player)sender).getInventory().addItem(stack);
+			} else {
+				sender.sendMessage("§cJe moet een dealer zijn!");
+			}
+		} else {
+			sender.sendMessage("§cGeen Rechten!");
+		}
+		return true;
+	}
+
+}
