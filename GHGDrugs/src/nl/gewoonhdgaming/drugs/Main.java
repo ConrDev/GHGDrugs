bf294f67b9fd631d6b44b5859ca4f18bad2be653
WTGConner <WTGConner@users.noreply.github.com>
2017-06-25 22:31:59 +0200

    Added Cocaïne

--- GHGDrugs/src/nl/gewoonhdgaming/drugs/Main.java
+++ GHGDrugs/src/nl/gewoonhdgaming/drugs/Main.java
@@ -2,7 +2,13 @@ package nl.gewoonhdgaming.drugs;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Main extends JavaPlugin {
 		
@@ -29,5 +35,20 @@ public class Main extends JavaPlugin {
 	        saveConfig();
 	        getLogger().info("Configuratie Herladen");
 	        Bukkit.broadcastMessage("De Sexuele Plugin is geladen");
+	}
+	    
+	@EventHandler
+	public void onInteract(PlayerInteractEvent event) {
+		if(event.getItem() != null) {
+			if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+				if(event.getItem().getType() == Material.SUGAR) {
+					if(event.getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§fCocaïne")) {
+						event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 2000, 5));
+						event.getPlayer().setWalkSpeed(0.1F);
+					}
+				}
+			}
+		}
+		
 	}
 }
